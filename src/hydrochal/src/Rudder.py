4244afd853ab30bc3472c02b057a1f318ca02deb
bettonga <gabriel.betton@gmail.com>
2023-12-15 11:54:10 +0100

    added minimum movement for servo

--- src/hydrochal/src/Rudder.py
+++ src/hydrochal/src/Rudder.py
@@ -6,10 +6,12 @@ import pigpio
 
 def servo_callback(msg, pi, pin):
     # Contrôler le servo en fonction de la valeur du message reçu
+    global last_cmd_sent
     cmd = msg.data
-    print("cmd : ",cmd)
-    pi.set_servo_pulsewidth(pin, cmd)
-    print("read : ",pi.read(pin))
+    if (abs(cmd-last_cmd_sent)>50):     # minimum de mouvement
+        # print("cmd sent : ",cmd)
+        pi.set_servo_pulsewidth(pin, cmd)
+        last_cmd_sent = cmd
 
 def main():
     rclpy.init()
@@ -21,14 +23,14 @@ def main():
     MAX_WIDTH=2000
 
     pi = pigpio.pi()
+    global last_cmd_sent
+    last_cmd_sent = 1500
 
     # Créer un abonné pour le topic "/cmd_safran"
     subscription = node.create_subscription(Int32, '/cmd_safran', lambda msg: servo_callback(msg, pi, NUM_GPIO), 10)
 
     try:
-        print("yoy1")
         rclpy.spin(node)
-        print("yoy2")
     except KeyboardInterrupt:
         pass
 
