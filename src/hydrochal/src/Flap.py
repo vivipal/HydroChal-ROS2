38b83a8dfab664ac5b5dec91b7e4f601788b2f2b
bettonga <gabriel.betton@gmail.com>
2024-02-02 15:29:12 +0100

    flap controller

---
+++ src/hydrochal/src/Flap.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import rclpy
+from std_msgs.msg import Float32
+import serial
+import sys
+
+def flap_callback(msg, serial_port):
+    # Contrôler le flap en fonction de la valeur du message reçu
+    # print("cmd_flap received: ", msg.data)
+    cmd_flap = int(1500 + 500*(msg.data/30.0))
+    cmd_flap = str(max(1000, min(2000, cmd_flap)))+ "\n"
+    serial_port.write(cmd_flap.encode('utf-8'))
+
+def main():
+    rclpy.init()
+    node = rclpy.create_node('flap_controller')
+    if len(sys.argv) != 2:
+        port = '/dev/ttyUSB0'
+        print("No port specified, using default port: /dev/ttyUSB0")
+    else:
+        port = sys.argv[1]
+        print("Using port: ", port)
+
+    # Configurer la communication série
+    serial_port = serial.Serial(port, baudrate=57600, timeout=1)
+
+    # Créer un abonné pour le topic "/cmd_flap"
+    subscription = node.create_subscription(Float32, '/cmd_flap', lambda msg: flap_callback(msg, serial_port), 10)
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    # Fermer la communication série et nettoyer
+    serial_port.close()
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__': 
+    main()
--- src/hydrochal/src/Rudder.py
+++ src/hydrochal/src/Rudder.py
@@ -8,19 +8,13 @@ def servo_callback(msg, pi, pin):
     # Contrôler le servo en fonction de la valeur du message reçu
     global last_angle_sent  # en deg
     angle_safran = max(-55, min(55, msg.data))
-    print("cmd received : ", msg.data)
+    # print("cmd received : ", msg.data)
     cmd = int(1497 +8.17*angle_safran -0.0187*angle_safran**2)
     cmd = max(1000, min(2000, cmd))
     if (abs(angle_safran-last_angle_sent)>2.5):     # minimum de mouvement de 5deg
-        print("cmd sent : ",cmd)
+        # print("cmd sent : ",cmd)
         pi.set_servo_pulsewidth(pin, cmd)
         last_angle_sent = angle_safran
-
-# def servo_callback(msg, pi, pin):
-#     # Contrôler le servo en fonction de la valeur du message reçu
-#     cmd = max(1000, min(2000, msg.data))
-#     print("cmd sent : ",cmd)
-#     pi.set_servo_pulsewidth(pin, cmd)
 
 def main():
     rclpy.init()
@@ -37,7 +31,7 @@ def main():
     last_angle_sent = 0
 
     # Créer un abonné pour le topic "/cmd_safran" en deg
-    subscription = node.create_subscription(Float32, '/cmd_safran', lambda msg: servo_callback(msg, pi, NUM_GPIO), 10)
+    subscription = node.create_subscription(Float32, '/cmd_rudder', lambda msg: servo_callback(msg, pi, NUM_GPIO), 10)
 
     try:
         rclpy.spin(node)
