cd020fac5d56ea8cf93cdc836aac9797aac1ec22
VP <virgile.pelle@ensta-bretagne.org>
2024-02-04 16:03:06 +0100

    new imu node

--- src/hydrochal/src/IMU.cpp
+++ src/hydrochal/src/IMU.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <sstream>
 #include <ctime>
+#include <fcntl.h> 
+#include <termios.h> 
 
 
 class IMUNode : public rclcpp::Node {
@@ -16,6 +18,58 @@ class IMUNode : public rclcpp::Node {
 
     rclcpp::Publisher<interfaces::msg::YPR>::SharedPtr publisher_;
 };
+
+void set_baudrate(int fd){
+  struct termios options;
+
+  //Get the current options for the port...
+  tcgetattr(fd, &options);
+
+  //Set the baud rates to 4800...
+  cfsetispeed(&options, B57600);
+  cfsetospeed(&options, B57600);
+
+  // Enable the receiver and set local mode...
+  options.c_cflag |= (CLOCAL | CREAD);
+
+  // Set 8N1
+  options.c_cflag &= ~PARENB;
+  options.c_cflag &= ~CSTOPB;
+  options.c_cflag &= ~CSIZE;
+  options.c_cflag |= CS8;
+
+  // Read data as raw
+  options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+  // Set the new options for the port...
+  tcsetattr(fd, TCSANOW, &options);
+}
+
+void receive_message(int fd,char *receivedMessage){
+  bool end_message = false;
+  char buffer[2];
+  while (!end_message){
+    read(fd,buffer,1);
+    if (*buffer=='\n'){
+      end_message = true;
+    }else{
+      strcat(receivedMessage,buffer);
+    }
+  }
+}
+
+
+
+void wait_new_message(int fd){
+  char buffer[2];
+
+  // wait for begin of a sentence ie '#'
+  do {
+    read(fd,buffer,1);
+  } while(*buffer!='#');
+
+}
+
 
 
 
@@ -28,56 +82,43 @@ int main(int argc, char const *argv[]) {
 
   std::string device_path = argv[1];
 
-  std::ifstream serialStream(device_path);
-
-  if (!serialStream.is_open()) {
-    std::cerr << "Error opening serial port" << std::endl;
+  int fd = open(device_path.c_str(), O_RDONLY | O_NOCTTY  );
+  if (fd < 0) {
+    std::cerr << "Error opening serial port: " << device_path << std::endl;
     return 1;
-  }else{
-    std::cout << "Serial port openned: " << device_path << '\n';
   }
+  
+  fcntl(fd, F_SETFL,0);
+  set_baudrate(fd);
 
+  std::cout << "Serial port openned: " << device_path << '\n';
 
   rclcpp::init(argc,argv);
   auto n = std::make_shared<IMUNode>();
 
-  int c = 0;
+  double yaw, pitch,roll;
 
-  std::string line;
-  std::getline(serialStream, line);
-  while (rclcpp::ok()) {
+  while (rclcpp::ok()){
+    wait_new_message(fd);
 
-    std::getline(serialStream, line);
-    std::cout << line << std::endl;
-    if (!line.empty()){
-      c++;
+    char receivedMessage[200] = {0};
+    receive_message(fd,receivedMessage);
 
-      std::vector<std::string> values;
-      std::stringstream ss(line);
-      std::string val;
-      std::getline(ss, val, '='); // read the "YPR="
+    if (sscanf(receivedMessage, "YPR=%lf,%lf,%lf", &yaw, &pitch, &roll) == 3) {
 
+      // std::cout << "Yaw: " << yaw << ", Pitch: " << pitch << ", Roll: " << roll << std::endl;
+      
       interfaces::msg::YPR msg;
+      msg.yaw = yaw;
+      msg.pitch = pitch;
+      msg.roll = roll;
 
-      std::getline(ss,val,',');
-      msg.yaw = std::stof(val);
-      std::getline(ss,val,',');
-      msg.pitch = std::stof(val);
-      std::getline(ss,val,',');
-      msg.roll = std::stof(val);
-
-
-      if ( c >= 5){
-        n->publisher_->publish(msg);
-        c = 0;
-      }
-
-
-
+      n->publisher_->publish(msg);
+    }else{
+      std::cerr << "Error parsing values from the line: " << std::endl;
+      std::cout <<  std::endl << "----\n";
     }
   }
-
-  serialStream.close();
 
   return 0;
 }
