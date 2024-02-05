292ada88c2a737e75dbaf43e4b374028fc59a559
unknown <joha155.gonzalez@gmail.com>
2024-02-05 13:53:30 +0100

    modif regu

--- src/hydrochal/src/regulation.py
+++ src/hydrochal/src/regulation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
+from numpy import cos,sin
 from numpy.linalg import norm,det
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
@@ -32,25 +33,25 @@ def rot2H(a):
 
 #-------------------------------
    
-def f(x,u):
+def f(self,x,u,awind,ψ):
     x,u=x.flatten(),u.flatten()
     θ=x[2]; v=x[3]; w=x[4]; δr=u[0]; u2=u[1]; δs=x[5];
     w_ap = np.array([[awind*np.cos(ψ-θ) - v*np.cos(ψ-θ)],[awind*np.sin(ψ-θ)]])
     ψ_ap = angle(w_ap)
     a_ap=np.linalg.norm(w_ap)
-    fr = p4*v*np.sin(δr)
-    fs = p3*(a_ap)* np.sin(δs-ψ_ap)
-    dx=v*np.cos(θ) + p0*awind*np.cos(ψ)
-    dy=v*np.sin(θ) + p0*awind*np.sin(ψ)
-    dv=(fs*np.sin(δs)-fr*np.sin(δr)-p1*v**2)/p8
-    dw=(fs*(p5-p6*np.cos(δs)) - p7*fr*np.cos(δr) - p2*w*v)/p9
+    fr = self.p4*v*np.sin(δr)
+    fs = self.p3*(a_ap)* np.sin(δs-ψ_ap)
+    dx=v*np.cos(θ) + self.p0*awind*np.cos(ψ)
+    dy=v*np.sin(θ) + self.p0*awind*np.sin(ψ)
+    dv=(fs*np.sin(δs)-fr*np.sin(δr)-self.p1*v**2)/self.p8
+    dw=(fs*(self.p5-self.p6*np.cos(δs)) - self.p7*fr*np.cos(δr) - self.p2*w*v)/self.p9
     ff=(a_ap)* np.sin(δs-u2-ψ_ap)
-    dδs = p10*ff*np.cos(u2)+   fs/p3 #=> voile
+    dδs = self.p10*ff*np.cos(u2)+   fs/self.p3 #=> voile
     xdot=np.array([[dx],[dy],[w],[dv],[dw],[dδs]])
     return xdot,ψ_ap,a_ap,fr,fs,ff
 
 
-def regulateur (x,q,a,b):
+def regulateur (self,x,q,a,b):
     psi=2
     zeta=np.pi/4
     theta=x[2]
@@ -58,18 +59,18 @@ def regulateur (x,q,a,b):
     f=np.hstack(((b-a)/np.linalg.norm(b-a),m-a))
     e=np.linalg.det(f)   
     phi=np.arctan2(b[1,0]-a[1,0],b[0,0]-a[0,0])
-    if abs(e)>r/2 :
+    if abs(e)>self.r/2 :
         q=np.sign(e)
     #theta_etoile=phi - (2*0.35/3.14)*arctan(e/r)
-    theta_etoile=phi - np.arctan(e/r)
-    if (((np.cos(psi-theta_etoile)+ np.cos(zeta)) < 0) or (abs(e)-r < 0 and (np.cos(psi-phi) + np.cos(zeta)) < 0)):
+    theta_etoile=phi - np.arctan(e/self.r)
+    if (((np.cos(psi-theta_etoile)+ np.cos(zeta)) < 0) or (abs(e)-self.r < 0 and (np.cos(psi-phi) + np.cos(zeta)) < 0)):
         thetabar=np.pi+psi-zeta*q
     else :
         thetabar=theta_etoile    
     if np.cos (thetabar-theta)>0:
-        sigma_r=sigma_r_max*np.sin(theta-thetabar)
+        sigma_r=self.sigma_r_max*np.sin(theta-thetabar)
     else :
-        sigma_r=sigma_r_max*np.sign(np.sin(theta-thetabar))
+        sigma_r=self.sigma_r_max*np.sign(np.sin(theta-thetabar))
     #sigma_max=1
     sigma_s_max = np.pi/2*((np.cos(psi-thetabar)+1)/2)**q
     
@@ -86,20 +87,21 @@ class Plannif_regulation(Node):
         super().__init__('sailboat_publisher')
 
         # Constants
-        self.p0 = 0.1
-        self.p1 = 50
-        self.p2 = 6000
-        self.p3 = 1000
-        self.p4 = 2000
-        self.p5 = 0.01
-        self.p6 = 1
-        self.p7 = 2
-        self.p8 = 300
-        self.p9 = 10000
-        self.p10 = 1
-        self.r = 10
-        self.sigma_r_max = 5
-    
+        self.p0 = 0.1 #dérive
+        self.p1 = 50 #frottement du bateau
+        self.p2 = 6000 #frottement à la rotation
+        self.p3 = 1000 # coef voile
+        self.p4 = 2000 # coef gouvernail
+        self.p5 = 0.01  #distance centre de poussée de la voile au mat (m)
+        self.p6 = 1 #distance quille mât (m)
+        self.p7 = 2 # distance gouvernail-quille (m)
+        self.p8 = 300 #masse (kg)
+        self.p9 = 10000 # moment d'inertie
+        self.p10 = 1 #coef flag
+        self.r = 10 # Taille couloirs
+        self.sigma_r_max = np.radians(55) # angle max du safran
+        self.dt= 0.1
+
         msg = Float64()
 
         self.coord_subscriber = self.create_subscription(GPS, 'GPS', self.coord_callback, 10)
@@ -113,9 +115,6 @@ class Plannif_regulation(Node):
             Float64, 
             'Commande', 
             10)
-
-
-        #self.subscription  # prevent unused variable warning
         
         # Init Variables de classe
 
@@ -126,8 +125,6 @@ class Plannif_regulation(Node):
         self.heading= 0.0 # Cap véhicule
         
         self.yaw=0.0 # angle voile
-
-
 
     def coord_callback(self, msg : GPS):
         self.P=msg.GPS
@@ -143,23 +140,23 @@ class Plannif_regulation(Node):
 
     def main_loop(self):
 
-        q=1
+        q=1 # Initialisation
         # VARIABLE PROGRAMME
-        ax=init_figure(-150,150,-80,80)
         i=0
-        x = np.array([P[0,0],P[1,0],heading,P[2,0],0,yaw]).reshape(6,1)   #x=(x,y,θ,v,w,δs)
-        # COORDONEE
+        x = np.array([self.P[0,0],self.P[1,0],self.heading,self.P[2,0],0,self.yaw]).reshape(6,1)   #x=(x,y,θ,v,w,δs)
+        # COORDONEE DES LIGNES A SUIVRE
+        
         a = np.array([[75],[-75]])   
         b = np.array([[-75],[-75]])
         c=np.array([-75,20]).reshape(2,1)
         d=np.array([-150,20]).reshape(2,1)
         coord=[a,b,c,d]
-        n=len(coord)
-        ψ_ap=vent[2,0]
-        sigma_r_max =90 # angle max du gouvernail
-        awind,ψ = vent[3,0],vent[1,0] # vitesse du vent, angle du vent 
+        
+        n=len(coord) # nbr de ligne
+        ψ_ap=self.vent[2,0]
+        sigma_r_max =45 # angle max du gouvernail
+        awind,ψ = self.vent[3,0],self.vent[1,0] # vitesse du vent, angle du vent 
 
-    def control (i,x,q) :
         while True :
             u,m=regulateur(x,q,coord[i],coord[i+1])
             vector_director = coord[i+1] - coord[i]
@@ -192,17 +189,18 @@ class Plannif_regulation(Node):
             if i==n-1:
                 break
 
-            u_msg = regulations()
+            u_msg = Plannif_regulation()
             u_msg.u_rudder= u[0,0]
             u_msg.u_flap= u[1,0]
             self.u_publisher_.publish(u_msg)
+            
+            xdot,ψ_ap,w_ap,fr,fs,ff=f(x,u)
+            x = x + self.dt*xdot
 
 
 def main(args=None):
     rclpy.init(args=args)
     regulation = Plannif_regulation()
-
     rclpy.spin(regulation)
-
     regulation.destroy_node()
     rclpy.shutdown()
